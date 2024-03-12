a21e5530b18e83917d94a6f66e26998fabb1b18f
Hanief Nashrullah <hanieffatkhan@gmail.com>
2024-03-12 22:05:01 +0700

    mbuh mumet

--- server.js
+++ server.js
@@ -1,37 +1,59 @@
-const express = require('express')
-const pool = require('./db')
+const express = require('express');
+const fs = require('fs');
+const pool = require('./db');
+const path = require('path');
 const port = 3000
-
 const app = express()
 app.use(express.json())
 
+
 app.get('/', async (req, res) => {
     try {
-        const data = await pool.query('SELECT * FROM movies')
-        res.status(200).send(data.rows)
+        res.status(200).sendFile(path.join(__dirname,'index.html'))
     } catch (err) {
         console.log(err)
         res.sendStatus(500)
     }
 })
 
-app.post('/', async (req, res) => {
-    const { Title, Year, imdbID, Type, Poster } = req.body
+app.get('/movie-data', async (req, res) => {
     try {
-        await pool.query('INSERT INTO movies (Title, Year, imdbID, Type, Poster) VALUES ($1, $2, $3, $4, $5)', [Title, Year, imdbID, Type, Poster])
-        res.status(200).send({ message: "Successfully added Titles" })
+        const data = await pool.query('SELECT * FROM movies')
+        res.status(200).json(data.rows)
     } catch (err) {
         console.log(err)
         res.sendStatus(500)
     }
 })
+
 
 app.get('/setup', async (req, res) => {
+    const data = fs.readFileSync('movies.json', 'utf8');
+    const movies = JSON.parse(data);
+    console.log(movies);
     try {
-        await pool.query('CREATE TABLE movies( Title VARCHAR(255), Year INT, imdbID VARCHAR(255), Type VARCHAR(255), Poster VARCHAR(255))')
-        res.status(200).send({ message: "Successfully created table" })
+        await pool.query('CREATE TABLE movies( Title VARCHAR(255), Year VARCHAR(255), imdbID VARCHAR(255), Type VARCHAR(255), Poster VARCHAR(255))')
+        for (let movie of movies) {
+            const { Title, Year, imdbID, Type, Poster } = movie;
+            await pool.query('INSERT INTO movies (Title, Year, imdbID, Type, Poster) VALUES ($1, $2, $3, $4, $5)', [Title, Year, imdbID, Type, Poster]);
+        }
+        res.status(200).send({ message: "Successfully added movie" })
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
     }
-    catch (err) {
+})
+
+
+app.post('/', async (req, res) => {
+    const movies = req.body
+    try {
+        for (let movie of movies) {
+            const { Title, Year, imdbID, Type, Poster } = movie;
+            await pool.query('INSERT INTO movies (Title, Year, imdbID, Type, Poster) VALUES ($1, $2, $3, $4, $5)', [Title, Year, imdbID, Type, Poster]);
+        }
+        res.status(200).send({ message: "Successfully added movie" })
+    } catch (err) {
         console.log(err)
         res.sendStatus(500)
     }
